a4388c727fabf26c910b8450bd5797ea23c63189
Ihor Skliar <skliar.ihor@gmail.com>
2025-01-21 16:05:18 -0500

    upd

--- ios/ExpoIsMacOSModule.m
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(ExpoIsMacOSModule, NSObject)
-RCT_EXTERN_METHOD(isMacOS:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-@end
