bea2f38f20c5cbafc42111a4b151b919c771737e
Ihor Skliar <skliar.ihor@gmail.com>
2025-01-21 11:33:53 -0500

    cleanup

--- src/ExpoIsMacOSModule.ts
+++ src/ExpoIsMacOSModule.ts
@@ -1,12 +1,6 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { requireNativeModule } from 'expo';
 
-import { ExpoIsMacOSModuleEvents } from './ExpoIsMacOS.types';
-
-declare class ExpoIsMacOSModule extends NativeModule<ExpoIsMacOSModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
-}
+import { ExpoIsMacOSModule } from './ExpoIsMacOS.types';
 
 // This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoIsMacOSModule>('ExpoIsMacOS');
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // Reexport the native module. On web, it will be resolved to ExpoIsMacOSModule.web.ts
 // and on native platforms to ExpoIsMacOSModule.ts
 export { default } from './ExpoIsMacOSModule';
-export { default as ExpoIsMacOSView } from './ExpoIsMacOSView';
 export * from  './ExpoIsMacOS.types';
